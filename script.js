2d54d22f670d3c1c7a557d44d36cabd24c635c66
Ahmad Zafrullah <23Pstars@gmail.com>
2018-04-30 14:59:09 +0800

    add origin country prefix verification

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var waAPIUrl = 'https://api.whatsapp.com/send?phone=',
+var waOrigin = '62', /* kode negara */
+    waAPIUrl = 'https://api.whatsapp.com/send?phone=',
     waNumber = document.getElementById('wa-number');
 
 document.getElementById('submit').addEventListener('submit', function () {
-    waNumber = waNumber.value.match(/[0-9]+/g).join('');
+    waNumber = waNumber.value.match(/[0-9]+/g).join('').replace(/^0/g, '62');;
     chrome.tabs.create({
         url: waAPIUrl + waNumber
     })
